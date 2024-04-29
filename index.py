fb714c98d64a9ecf559ad5b00b4a1a9a1a2b8bda
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-04-29 15:38:11 -0400

    lab complete

---
+++ index.py
@@ -0,0 +1,3 @@
+from lib.MySet import MySet
+
+set1 = MySet(list=[1,2,3,4,5,6])
--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,37 @@
 class MySet:
 
-    pass
+    def __init__(self, list = []):
+        self.dictionary = {}
+        for value in list:
+            self.dictionary[value] = True
+    def __repr__(self):
+        return f"MySet({self.dictionary})"
+    
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def move(self, value1, value2):
+    # value2 is the key to be moved, value1 is the value where value2 will be next to
+        value_to_move = self.dictionary.pop(value2)
+        new_dict = {}
+        for key, value in self.dictionary.items():
+            new_dict[key] = value
+            if key == value1:
+                new_dict[value2] = value_to_move
+        self.dictionary = new_dict
+    def __str__(self):
+        return f"MySet: {{{(','.join(map(str, self.dictionary.keys())))}}}"
+
+    def clear(self):
+        self.dictionary = {}
